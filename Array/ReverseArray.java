ea6ff3eb1407daada749e3c9680628842c26f4a7
alisha-dongare-comprinno <alisha.dongare@comprinno.net>
2026-02-25 12:45:32 +0530

    Update ReverseArray.java

--- Array/ReverseArray.java
+++ Array/ReverseArray.java
@@ -1,18 +1,19 @@
 public class ReverseArray{
     public static void main(String[] args){
-       int a[]={1,2,3,4,5,6,7}
-       int i=0; j=a.length; temp;
+       int a[]={1,2,3,4,5,6,7};
+       int (i=0; j=a.length; temp);
 
-        while(i<j);
+        while(i<j)
         {
-            temp=a[i];
-            a[i]=a[j];
-            temp=a[j];
+            temp = a[i];
+            a[i] = a[j];
+            a[j] = temp;
             i++;
             j--;
         } 
-        System.out.println(array.toString(a));
+        System.out.println(Arrays.toString(a));
     
   }   
 
 }
+
